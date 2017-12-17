4416436b4d1c36ce0e81711286d797e8620be56a
nicholas.robbins <nicholas.robbins@mymail.champlain.edu>
2017-12-17 11:33:16 -0500

    Started board game movement

---
+++ Systems Practice/Assets/Scripts/BoardMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI
+using UnityEngine;
+
+public class BoardMovement : MonoBehaviour {
+
+	int mDiceCounter;
+	public TextMesh mDice;
+	// Use this for initialization
+	void Start () {
+		mDiceCounter = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		increaseDice();
+
+
+
+	}
+
+
+
+	void increaseDice()
+	{
+		if(mDiceCounter < 6)
+		{
+			mDiceCounter++;
+		}
+		else
+		{
+			mDiceCounter = 0;
+		}
+
+		mDice.text = mDiceCounter;
+	}
+}
---
+++ Systems Practice/Assets/Scripts/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connection : MonoBehaviour {
+	[SerializeField]
+	Node mFrom;
+	[SerializeField]
+	Node mTo;
+	[SerializeField]
+	float mConnectionCost;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	public Node getFrom()
+	{
+		return mFrom;
+	}
+
+	public Node getTo()
+	{
+		return mTo;
+	}
+
+	public float getConnectionCost()
+	{
+		return mConnectionCost;
+	}
+}
